0a7f17e4074c331277a41fcdb297aa987813e9d9
Dmytro-Hladkykh <152580956+Dmytro-Hladkykh@users.noreply.github.com>
2023-12-04 11:14:04 +0200

    Add files via upload

--- Project_Kafka/Project_Kafka/create_topic.sh
+++ Project_Kafka/Project_Kafka/create_topic.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker exec -it project_kafka-kafka-1 kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 1 --topic bitcoin_transactions
+docker exec -it kafka-server kafka-topics.sh --create --bootstrap-server kafka:9092 --replication-factor 1 --partitions 1 --topic bitcoin_transactions
